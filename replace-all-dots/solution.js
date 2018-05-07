6e736829bcf0454e9c042bd9545ca5e9fc2ee590
Amy Cohen <anbcohen@gmail.com>
2018-05-07 14:15:26 -0700

    found the solution

--- replace-all-dots/solution.js
+++ replace-all-dots/solution.js
@@ -1,16 +1,8 @@
-//solution does not work, but I'm not sure how to submit this for partial credit without doing this incorrect solution.
-
 var replaceDots = function(str) {
-  for (var i = 0; i < str.length; i++){
-    if (str.includes('U+002E')) {
-      str.replace ('U+002E', 'U+06D4');
-    } else {
-      console.log('you\'re missing something');
-    }
-  }
-  return str.replace ('U+002E', 'U+06D4');
-
+  var stringArray = str.split('.');
+  var joinedString = stringArray.join('-');
+  console.log(joinedString);
+  return joinedString;
 };
-
 
 replaceDots('one.two.three');
