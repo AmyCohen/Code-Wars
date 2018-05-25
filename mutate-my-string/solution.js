9e25db4bf4bf86aacc9987518825df93db93b3fc
Amy Cohen <anbcohen@gmail.com>
2018-05-24 22:45:07 -0700

    completed kata CORRECTLY

--- mutate-my-string/solution.js
+++ mutate-my-string/solution.js
@@ -1,18 +1,15 @@
-//This is not functioning code. I could not get it to work yet.
 function mutateMyStrings(stringOne, stringTwo){
-  let revisedStr = "";
-  let addingStr = "";
-  for (let i = 0; i < stringOne.length; i++) {
-    let letterSub = "";
-    let otherLetterSub = "";
-    letterSub = stringTwo.substr(i, 1)
-    addingStr = letterSub
-    console.log(addingStr + letterSub);
+  let arrayOne = stringOne.split('');
+  let arrayTwo = stringTwo.split('');
+  let solutionArray = [];
 
-    // console.log(revisedStr);
-    otherLetterSub = stringOne.slice(i+1)
-    console.log(otherLetterSub);
-    revisedStr = letterSub+otherLetterSub;
-    console.log(revisedStr);
+  for (let i = 0; i < arrayOne.length; i++) {
+    if (arrayOne[i] !== arrayTwo[i]) {
+    arrayOne.splice([i], 1, arrayTwo[i]);
+    solutionArray.push(arrayOne.join('')+'\n');
   }
 }
+  return stringOne + '\n' + solutionArray.join('');
+}
+
+//Okay...so I don't know where my brain was yesterday (I litterally fell asleep standing up at home and my husband caught me - I wasn't in a good place physically yesterday), but SERIOUSLY I tried keeping this as a string instead of using the much easier array methods then rejoining the array. SHEESH. This solution (once I had slept) took about 30 minutes is all. GAH!!!
